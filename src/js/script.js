a8fcabb758e8ae284290c607683a4dfa78952bd8
Andrey Trushnikov <at@solar.bi>
2020-06-17 17:54:55 +0500

    1. Создан проект. 2. Добавлены иконки флага и закрытой ячейки 3. Прописаны функции: "Запуск", "Таймер", "Проверка на мину", "Создание массива с минами", "Проверка на то, сколько осталось бомб". 4. Отслежены клики по Старту, Ячейкам. 5. Отслежен правый клик по ячейке с установкой флажка

---
+++ src/js/script.js
@@ -0,0 +1,142 @@
+/* Сапёр 9*9 клеток, 10 мин */
+
+let count = 0;
+let tablo = document.querySelector('#tablo'); // Табло с оставшимися минами
+let startReset = document.getElementById('reset'); // Кнопка начала-сброса
+let cellsBlock = document.getElementById('miner-body') // Отец всех полей с возможными минами
+let cells = document.getElementsByClassName('miner-cell') // Множество всех полей с возможными минами
+let arr = []; // Массив с ячейками, содержащими мины
+let timer = document.getElementById("timer"); // Input с таймером
+let timerId; // ID таймера
+
+const minesCount = 10; // Количество мин
+const minCell = 1;     // от 1 ячейки
+const maxCell = 81;    // до 81 ячейки
+
+let tempMines = 0;
+
+console.log(arr);
+
+// Функция запуска игры
+function start(e) {
+
+    if (startReset.value == 'Start') {
+        timerId = setInterval(() => startTimer(), 1000);
+        startReset.value = 'Reset';
+        arr = randomMines(minCell, maxCell, minesCount); // Создание массива с минами
+    } else if (startReset.value == 'Reset') {
+        timer.value = '000';
+        clearInterval(timerId);
+        startReset.value = 'Start';
+        arr = []
+    }
+
+    for (element in cells) {
+        if (cells.hasOwnProperty(element)) {
+             cells[element].classList.remove('miner-cell--error') 
+        }
+    }
+
+    console.log(arr);
+    return arr
+}
+
+// Таймер в хедере
+function startTimer() {
+    timer.value++
+
+    if (timer.value<10) {
+        timer.value = ''+0+0+timer.value
+    }
+    if ((timer.value>=10) && (timer.value<100)) {
+        timer.value = ''+0+timer.value
+    }
+    document.getElementById("timer").setAttribute('value',  timer.value);
+}
+
+// Проверка на мину
+function checkMine(target, arr) {
+    let id = target.getAttribute('data-id');
+
+    if (id == null) return
+    console.log('id =',id);
+    for (element in arr) {
+        // debugger
+        if (arr[element] == id ) {
+            target.classList.add('miner-cell--error')
+
+            console.log('Бабах! Ячейка с миной = ', id);
+        }
+    }
+}
+
+// Создание массива с минами
+function randomMines(min, max, quantity) {
+    // Создание рандомного числа от мин до макс включая
+    function rand() {
+        return min - 0.5 + Math.random() * (max - min + 1)
+    }
+    // Проверка на то, чтобы не было одинаковых рандомных чисел
+    function checkTheSameRand() {
+        tempRand = Math.round(rand())
+        if (arr.includes(tempRand)) checkTheSameRand()
+        return tempRand
+    }
+
+    for (i=0; i<=quantity; i++) {
+        arr[i] = checkTheSameRand()
+    }
+    return arr
+}
+
+// Проверка того, сколько осталось бомб (Нужна проверка для случаев победы или неправильно расположения мин)
+function checkNumberOfBombs() {
+    for (element in cells) {
+        if (cells.hasOwnProperty(element)) {
+           if (cells[element].classList.contains('miner-cell--bomb')) {
+                tempMines++
+           }
+        }
+        tablo.value = minesCount - tempMines
+    }
+    tempMines = 0;
+}
+// Проверки пустых ячеек
+// function checkCell(x,y) {
+//     if ((x == 1) && (y == 1)) {
+
+//     }
+// }
+
+// Клик по старту
+startReset.addEventListener('click', function(e) {
+    e.preventDefault();
+    start(e)
+})
+// Клик по ячейке
+cellsBlock.addEventListener('click', function(e) {
+    e.preventDefault();
+    let x = e.target.getAttribute('data-x');
+    let y = e.target.getAttribute('data-y');
+
+    if (startReset.value == 'Start') {
+        start()
+        checkMine(e.target, arr)
+    } else {
+        checkMine(e.target, arr);
+    }
+    console.log(e.target);
+})
+
+// Правый клик по ячейке - установка флажка
+cellsBlock.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    e.target.classList.toggle('miner-cell--bomb');
+    if (startReset.value == 'Start') {
+        start()
+        checkNumberOfBombs();
+    } else {
+        checkNumberOfBombs();
+    }
+    return false;
+}, false)
