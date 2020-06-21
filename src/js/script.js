dd2ecfb8c915c84fcd395db4755591a956fb7db4
Andrey Trushnikov <fingerflip@mail.ru>
2020-06-22 00:03:41 +0500

    Разбил поле на сектора с разными проверками

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,4 @@
-/*
-    *
-        *
-            * Сапёр 9*9 клеток, 10 мин *
-        *
-    *
-*/
+/* Сапёр 9*9 клеток, 10 мин */
 
 let tablo = document.querySelector('#tablo'); // Табло с оставшимися минами
 let startReset = document.getElementById('reset'); // Кнопка начала-сброса
@@ -22,7 +16,7 @@ const minCell = 1;     // от 1 ячейки
 const maxCell = 81;    // до 81 ячейки
 const maxX = 9;
 const maxY = 9;
-
+let tempCell;
 let tempMines = 0;
 
 // Таймер в хедере
@@ -48,23 +42,23 @@ function loser() {
 }
 
 // Проверка на мину
-function checkMine(target, arrWithRandomMines) {
-    let id = target.getAttribute('data-id');
+// function checkMine(target, arrWithRandomMines) {
+//     let id = target.getAttribute('data-id');
 
-    if (id == null) {
-        return;
-    }
+//     if (id == null) {
+//         return;
+//     }
 
-    for (var element in arrWithRandomMines) {
-        // debugger
-        if (arrWithRandomMines[element] == id ) {
-            target.classList.add('miner-cell--error');
-            console.log('Бабах! Ячейка с миной = ', id);
-            loser();
-            return;
-        }
-    }
-}
+//     for (var element in arrWithRandomMines) {
+//         // debugger
+//         if (arrWithRandomMines[element] == id ) {
+//             target.classList.add('miner-cell--error');
+//             console.log('Бабах! Ячейка с миной = ', id);
+//             loser();
+//             return;
+//         }
+//     }
+// }
 
 // Создание массива с минами
 function randomMines(min, max, quantity) {
@@ -81,7 +75,7 @@ function randomMines(min, max, quantity) {
         return tempRand;
     }
 
-    for (let i=0; i<=quantity; i++) {
+    for (let i=0; i<quantity; i++) {
         arrWithRandomMines[i] = checkTheSameRand();
     }
     return arrWithRandomMines;
@@ -102,14 +96,52 @@ function checkNumberOfBombs() {
 
 // Создание общего объекта со всеми ячейками и минами
 function minesObjFn(arrWithRandomMines,maxX,maxY) {
-    let fullObj = {};
+    minesObj = {};
     let x = 1;
     let y = 1;
     // console.log(arrWithRandomMines);
-
+    console.log(maxX*maxY - (maxX-1));
+    
     // создание пустого объекта ячеек
     for (var i=1; i<=maxX*maxY; i++) {
-        fullObj[i] = {"x": x,"y":y, mine: false};
+        let place;
+        // 1
+        if (i == '1') {
+            place = 'leftTopCorner';
+        }
+        // 2-8
+        else if ((i>1) && (i<maxX)) {
+            place = 'topHorizontalLine'; 
+        }
+        // 9
+        else if (i == maxX) {
+            place = 'rightTopCorner';
+        }
+        // 18,27,36 .. 72
+        else if ((i % maxX == 0) && (i != maxX) && (i != maxX*maxY)) {
+            place = 'rightVerticalLine';
+        }
+        // 10,19,28,37
+        else if (((i-1) % maxX == 0) && (i != maxX*maxY - (maxX-1))) {
+            place = 'leftVerticalLine';
+        }
+        // 73
+        else if (i == maxX*maxY - (maxX-1)) {
+            place = 'leftBottomCorner';
+        }
+        // 74-80
+        else if ((i> maxX*maxY - (maxX-1)) && (i<maxX*maxY)) {
+            place = 'bottomHorizontalLine'; 
+        }
+        // 81
+        else if (i == maxX*maxY) {
+            place = 'rightBottomCorner';
+        }
+        else {
+            place = 'center';
+        }
+
+        minesObj[i] = {"x": x,"y":y, mine: false,place: place};
         x++;
         if (x == 10) {
             y++;
@@ -118,63 +150,114 @@ function minesObjFn(arrWithRandomMines,maxX,maxY) {
     }
 
     // добавление мин
-    for (var el in fullObj) {
+    for (var el in minesObj) {
         if (arrWithRandomMines.includes(parseInt(el))) {
-            fullObj[el].mine = true;
+            minesObj[el].mine = true;
         }
     }
 
-    return fullObj;
+    return minesObj;
 }
 
 // Проверки пустых ячеек
 function checkCell(target, minesObj) {
     console.log(minesObj);
+    // проверка на мину
+    let cell = parseInt(target.getAttribute('data-id'));
     
-    let cell = target.getAttribute('data-id');
-    console.log(minesObj[cell]);
-    
-    let x = minesObj[cell].x;
-    let y = minesObj[cell].y;
-    let count = 0;
-    let siblingsObj = {
-        1: {"x": x-1,"y":y-1,mine:'',id:'',isCheck: false},
-        2: {"x": x  ,"y":y-1,mine:'',id:'',isCheck: false},
-        3: {"x": x+1,"y":y-1,mine:'',id:'',isCheck: false},
-        4: {"x": x-1,"y":y  ,mine:'',id:'',isCheck: false},
-        5: {"x": x+1,"y":y  ,mine:'',id:'',isCheck: false},
-        6: {"x": x-1,"y":y+1,mine:'',id:'',isCheck: false},
-        7: {"x": x  ,"y":y+1,mine:'',id:'',isCheck: false},
-        8: {"x": x+1,"y":y+1,mine:'',id:'',isCheck: false}
-    };
+    if (cell == null) {
+        return;
+    }
+    if (minesObj[`${cell}`].mine == true) {
+        target.classList.add('miner-cell--error');
+        console.log('Бабах! Ячейка с миной = ', cell);
+        loser();
+        return;
+    } else {
+        // console.log(minesObj[cell]);
+        // 1-9(max)
+        // 10,
 
-    console.log(siblingsObj);
+        if ( minesObj[cell].place == 'leftTopCorner') {
+            console.log('leftTopCorner');
+        }
+        if ( minesObj[cell].place == 'rightTopCorner') {
+            console.log('rightTopCorner');
+        }
+        if ( minesObj[cell].place == 'leftBottomCorner') {
+            console.log('leftBottomCorner');
+        }
+        if ( minesObj[cell].place == 'rightBottomCorner') {
+            console.log('rightBottomCorner');
+        }
+        if ( minesObj[cell].place == 'topHorizontalLine') {
+            console.log('topHorizontalLine');
+        }
+        if ( minesObj[cell].place == 'bottomHorizontalLine') {
+            console.log('bottomHorizontalLine');
+        }
+        if ( minesObj[cell].place == 'leftVerticalLine') {
+            console.log('leftVerticalLine');
+        }
+        if ( minesObj[cell].place == 'rightVerticalLine') {
+            console.log('rightVerticalLine');
+        }
+        if ( minesObj[cell].place == 'center') {
+            console.log('center');
+        }
+
+
+        let x = minesObj[cell].x;
+        let y = minesObj[cell].y;
+        let count = 0;
+        let siblingsObj = {
+            1: {"x": x-1,"y":y-1,mine:'',id:'',isCheck: false},
+            2: {"x": x  ,"y":y-1,mine:'',id:'',isCheck: false},
+            3: {"x": x+1,"y":y-1,mine:'',id:'',isCheck: false},
+            4: {"x": x-1,"y":y  ,mine:'',id:'',isCheck: false},
+            5: {"x": x+1,"y":y  ,mine:'',id:'',isCheck: false},
+            6: {"x": x-1,"y":y+1,mine:'',id:'',isCheck: false},
+            7: {"x": x  ,"y":y+1,mine:'',id:'',isCheck: false},
+            8: {"x": x+1,"y":y+1,mine:'',id:'',isCheck: false}
+        };
     
-    // Собираем новый объект с соседями, с указателями на мину
-    for (let mineEl in minesObj) {
-        if (minesObj.hasOwnProperty(mineEl)) {
-            for (let newMineEl in siblingsObj) {
-                if (siblingsObj.hasOwnProperty(newMineEl)) {
-                    // Если есть мина в основном объекте, то добавляем id в новый объект
-                    console.log(minesObj[mineEl].mine);
-                    
-                    // if (minesObj[mineEl].mine == true) {
-                    //     siblingsObj[newMineEl].id = mineEl;
-                    // }
+        console.log('siblingsObj before',siblingsObj);
+        
+        // Собираем новый объект с соседями, с указателями на мину
+    
+        // if ()
+        for (let mineEl in minesObj) {
+            if (minesObj.hasOwnProperty(mineEl)) {
+                // console.log('mineEl',mineEl);
+                
+                for (let newMineEl in siblingsObj) {
+                    if (siblingsObj.hasOwnProperty(newMineEl)) {
+                        // console.log('newMineEl',newMineEl);
+                        
+                        // Если есть мина в основном объекте, то добавляем id в новый объект
+                        // console.log(minesObj[mineEl].mine);
+                        // siblingsObj[]
+                        // if (minesObj[mineEl].mine == true) {
+                        //     siblingsObj[newMineEl].id = mineEl;
+                        // }
+                    }
                 }
             }
         }
+    
+        console.log('siblingsObj after',siblingsObj);
+        
+    
+        // debugger
+        target.classList.add('miner-cell--free');
+    
+        // if (count == 0) {
+    
+        // }
+    
+        return count;
     }
-
-
-    // debugger
-    target.classList.add('miner-cell--free');
-
-    // if (count == 0) {
-
-    // }
-
-    return count;
+   
 }
 
 // Очистка поля 
@@ -197,13 +280,14 @@ function start() {
         startReset.value = 'Reset';
         arrWithRandomMines = randomMines(minCell, maxCell, minesCount); // Создание массива с минами
         minesObj = minesObjFn(arrWithRandomMines,maxX,maxY);
-
+        
     } else if (startReset.value == 'Reset') {
         // Очищаем всё
         timer.value = '000';
         clearInterval(timerId);
         startReset.value = 'Start';
         arrWithRandomMines = [];
+        minesObj = {};
         clearField();
     }
 
@@ -235,7 +319,7 @@ cellsBlock.addEventListener('click', function(e) {
 
     if (startReset.value == 'Start') {
         start();
-        checkMine(e.target, arrWithRandomMines);
+        // checkMine(e.target, arrWithRandomMines);
         numberOfMines = checkCell(e.target, minesObj); // проверка ячейки и возврат количества мин рядом
 
         if ((numberOfMines == null) || (numberOfMines == 0)) {
@@ -244,7 +328,7 @@ cellsBlock.addEventListener('click', function(e) {
             e.target.innerHTML = numberOfMines;
         }
     } else {
-        checkMine(e.target, arrWithRandomMines);
+        // checkMine(e.target, arrWithRandomMines);
         numberOfMines = checkCell(e.target, minesObj); // проверка ячейки и возврат количества мин рядом
 
         if ((numberOfMines == null) || (numberOfMines == 0)) {
